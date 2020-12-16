29208ecac9053af467604b7a5cf2953148138659
Charles Reign <charlesgold45@gmail.com>
2020-12-16 04:00:44 +0000

    version 1.0.1

---
+++ App/Config.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+/**
+ * Application configuration
+ *
+ * PHP version 5.4
+ */
+class Config
+{
+
+    /**
+     * DATABASE HOST
+     * @var string
+     */
+    const DB_HOST = '';
+
+    /**
+     * DATABASE NAME
+     * @var string
+     */
+    const DB_NAME = '';
+
+    /**
+     * DATABASE USER
+     * @var string
+     */
+    const DB_USER = '';
+
+    /**
+     * DATABASE PASSWORD
+     * @var string
+     */
+    const DB_PASSWORD = '';
+
+    /**
+     * SHOW OR HIDE ALL MESSAGES
+     * @var boolean
+     */
+    const SHOW_ERRORS = true;
+}
